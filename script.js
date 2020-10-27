57741eda12a7b278e1f557b02825de8146cfaf76
kokocat1313 <66399733+kokocat1313@users.noreply.github.com>
2020-10-26 18:36:06 -0700

    1st commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const square = {
+    area(length){
+        return length * length
+    },
+    perimeter(length){
+        
+     return length * 4   
+    }
+}
+
+squre.area(10)
+square.perimeter(10)
